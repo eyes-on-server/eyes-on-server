73f5a4341c89ad3b78197281a9930f2703083157
unknown <felipe.almeida@sptech.school>
2023-08-18 17:25:41 -0300

    Adicionando tabelas de desenvolvedor e servidor

--- banco_de_dados/eye-on-server-script.sql
+++ banco_de_dados/eye-on-server-script.sql
@@ -14,17 +14,28 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 CREATE SCHEMA IF NOT EXISTS `eye-on-server` DEFAULT CHARACTER SET utf8 ;
 USE `eye-on-server` ;
 
+
+-- Tabela de Desenvolvedor
+
+CREATE TABLE IF NOT EXISTS `eye-on-server`.`desenvolvedor` (
+  `idDev` INT NOT NULL AUTO_INCREMENT,
+  `nomeDev` VARCHAR(120) NULL,
+  `emailDev` CHAR(120) NULL,
+  `senhaDev` VARCHAR(120) NULL,
+  PRIMARY KEY (`idDev`))
+ENGINE = InnoDB;
+
 -- -----------------------------------------------------
 -- Table `eye-on-server`.`empresa`
 -- -----------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS `eye-on-server`.`empresa` (
   `idEmpresa` INT NOT NULL AUTO_INCREMENT,
-  `nomeFantasia` VARCHAR(45) NULL,
+  `nomeFantasia` VARCHAR(120) NULL,
   `cnpj` CHAR(14) NULL,
-  `email` VARCHAR(45) NULL,
+  `email` VARCHAR(120) NULL,
   PRIMARY KEY (`idEmpresa`))
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `eye-on-server`.`usuario`
@@ -32,13 +43,32 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `eye-on-server`.`usuario` (
   `idusuario` INT NOT NULL AUTO_INCREMENT,
   `fkEmpresa` INT NULL,
-  `nome` VARCHAR(45) NULL,
+  `nome` VARCHAR(120) NULL,
   `root` INT NULL,
-  `senha` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
+  `senha` VARCHAR(120) NULL,
+  `email` VARCHAR(120) NULL,
   PRIMARY KEY (`idusuario`),
   INDEX `fk_usuario_empresa_idx` (`fkEmpresa` ASC) VISIBLE,
   CONSTRAINT `fk_usuario_empresa`
+    FOREIGN KEY (`fkEmpresa`)
+    REFERENCES `eye-on-server`.`empresa` (`idEmpresa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- Tabela de servidores
+
+CREATE TABLE IF NOT EXISTS `eye-on-server`.`servidor` (
+  `idServidor` INT NOT NULL AUTO_INCREMENT,
+  `fkEmpresa` INT NULL,
+  `nomeServidor` VARCHAR(120) NULL,
+  `tipoServidor` VARCHAR(120) NULL,
+  `descricaoServidor` VARCHAR(120) NULL,
+  `localServidor` VARCHAR(120) NULL,
+  PRIMARY KEY (`idServidor`),
+  INDEX `fk_servidor_empresa_idx` (`fkEmpresa` ASC) VISIBLE,
+  CONSTRAINT `fk_servidor_empresa`
     FOREIGN KEY (`fkEmpresa`)
     REFERENCES `eye-on-server`.`empresa` (`idEmpresa`)
     ON DELETE NO ACTION
