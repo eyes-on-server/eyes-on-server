97672f033397fb16d691c45c7ec25d3a70ffb0ce
giomenezes <giovanna.rodrigues@sptech.school>
2023-09-09 19:46:29 -0300

    Adicionando a API de captura e bot do slack

---
+++ botSlack/slack.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+mensagem = {
+    "text": f"""
+        ALERTA:
+        Sua CPU ultrapassou 92% de processamento!
+"""}
+
+chatCliente = "https://hooks.slack.com/services/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"
+
+postMsg = requests.post(chatCliente, data=json.dumps(mensagem))
+print(postMsg.status_code)
---
+++ capturaDados/chamadoJira.py
@@ -0,0 +1,52 @@
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://eyes-on-server.atlassian.net/rest/api/3/issue"
+
+auth = HTTPBasicAuth("eyes_on_server@outlook.com", "ATATT3xFfGF07cwan7moJwt8uMO-WA76cZX27MEb-b9NlnffWrJGxhU77Fnz4df4DViKFl9D8frWP2nfxL2cd2DclIXbtpa9m04DKChUZeiemi4baY9b5wV4PIVMxh5uA6pU5bVWCl7X__qF8pgNU-28ukytKuksB0FKCud4jqGZGl15drbU9dI=23F11F4E")
+
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+}
+
+payload = json.dumps( {
+  "fields": {
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "Order entry fails when selecting supplier.",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    'issuetype': {
+      'name': 'General requests'
+    },
+    "summary": "Main order flow broken",
+    "project": {
+        "key:" "EOS"
+    },
+  },
+  "update": {}
+} )
+
+response = requests.request(
+   "POST",
+   url,
+   data=payload,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
