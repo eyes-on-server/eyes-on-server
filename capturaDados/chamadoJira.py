bf67ee2530737f5f8d2d935d31731cb3f2b99dbd
giomenezes <giovanna.rodrigues@sptech.school>
2023-09-11 15:13:25 -0300

    fix(): Chamado de Jira e slack automaticos conforme a captura de dados e as KPIs

--- capturaDados/chamadoJira.py
+++ capturaDados/chamadoJira.py
@@ -3,56 +3,26 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+from jira import JIRA
 
+
+
+jira_token = "ATATT3xFfGF0qQSV8Yv2KCN5uGBfxwWardioua9TlmgdJfk_lXTxmHG0HYb71Ko7ETenQKKZuYB3hU0H4ig7Ip5iTwETBdMIIuMWe2ceHZwsp7ppUFog8L0r3u64xVicfpx2aW5ekSdm-O4VHL25--Poejq5grLO0ODrshAzooMcfEuExrKtDCc=EFD8BFBC"
 url = "https://eyes-on-server.atlassian.net/rest/api/3/issue"
+server_name = "https://eyes-on-server.atlassian.net"
+email = "eyes_on_server@outlook.com"
 
-auth = HTTPBasicAuth("eyes_on_server@outlook.com", "ATATT3xFfGF07cwan7moJwt8uMO-WA76cZX27MEb-b9NlnffWrJGxhU77Fnz4df4DViKFl9D8frWP2nfxL2cd2DclIXbtpa9m04DKChUZeiemi4baY9b5wV4PIVMxh5uA6pU5bVWCl7X__qF8pgNU-28ukytKuksB0FKCud4jqGZGl15drbU9dI=23F11F4E")
-
-headers = {
-  "Accept": "application/json",
-  "Content-Type": "application/json"
-}
-
-# Erro: Exception has occurred: TypeError
-# Object of type set is not JSON serializable
-#   File "/home/gio/Codigos/EOS/repositorio_central/capturaDados/chamadoJira.py", line 16, in <module>
-#     payload = json.dumps( {
-#               ^^^^^^^^^^^^^
-# TypeError: Object of type set is not JSON serializable
-payload = json.dumps( {
-  "fields": {
-    "description": {
-      "content": [
-        {
-          "content": [
-            {
-              "text": "Order entry fails when selecting supplier.",
-              "type": "text"
-            }
-          ],
-          "type": "paragraph"
-        }
-      ],
-      "type": "doc",
-      "version": 1
-    },
-    'issuetype': {
-      'name': 'General requests'
-    },
-    "summary": "Main order flow broken",
-    "project": {
-        "key:" "EOS"
-    },
-  },
-  "update": {}
-} )
-
-response = requests.request(
-   "POST",
-   url,
-   data=payload,
-   headers=headers,
-   auth=auth
+jira_connection = JIRA(
+  basic_auth=(email, jira_token),
+  server=server_name
 )
 
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+def chamado(mensagem, descricao):
+  issue_dict = {
+    'project': {'key': 'EOS'},
+    'summary': mensagem,
+    'description': descricao,
+    'issuetype': {'id': '10007'},
+  }
+
+  new_issue = jira_connection.create_issue(fields=issue_dict)
