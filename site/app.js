141c8e85c52b5d04fb109921179c3a360fc45b80
Davi-Hilario <davi.hilario@sptech.school>
2023-09-25 00:24:21 -0300

    mudando mensagem do servidor em execução

--- site/app.js
+++ site/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const porta = 5000;
@@ -11,13 +11,11 @@ const empresaRota = require("./src/routers/empresa");
 const userRota = require("./src/routers/user");
 const indexRota = require("./src/routers/index");
 
-
 // Configurações -----------------------
 app.use(express.json());
-app.use(express.urlencoded({extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
-
 
 // Criação de Rotas --------------------
 app.use("/", indexRota);
@@ -25,8 +23,7 @@ app.use("/user", userRota);
 app.use("/empresa", empresaRota);
 app.use("/servidor", servidorRota);
 
-
 // Iniciar servidor --------------------
 app.listen(porta, () => {
-    console.log(`http://localhost:${porta}/`)
-})
+  console.log(`Servidor rodando na porta ${porta}`);
+});
