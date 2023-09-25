fe0bbb2ef0da88892bc8f02015361c6684becff9
Davi-Hilario <davi.hilario@sptech.school>
2023-09-25 00:33:13 -0300

    alterando nomes de variaveis

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -1,34 +1,31 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-var configBd = {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+var sqlConfig = {
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 };
 
-function consultaBd(pesquisa) {
-    return new Promise((resolve, reject) => {
-        var conexao = mysql.createConnection(configBd);
-        conexao.connect();
+function consultaBd(instrucao) {
+  return new Promise((resolve, reject) => {
+    var conexao = mysql.createConnection(sqlConfig);
+    conexao.connect();
 
-        conexao.query(pesquisa, (erro, resultados) => {
-            conexao.end();
+    conexao.query(instrucao, (erro, resultados) => {
+      conexao.end();
 
-            if (erro) {
-                reject(erro);
-            }
+      if (erro) {
+        reject(erro);
+      }
 
-            resolve(resultados);
+      resolve(resultados);
+    });
 
-        });
-
-        conexao.on('error', function (erro) {
-            return ("ERRO NO MySQL: ", erro.sqlMessage)
-        })
-    
-    })
+    conexao.on("error", function (erro) {
+      return "ERRO NO MySQL: ", erro.sqlMessage;
+    });
+  });
 }
 
-
-module.exports = {consultaBd}
+module.exports = { consultaBd };
