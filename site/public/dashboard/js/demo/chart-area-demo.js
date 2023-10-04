d87df08b363d00b3aff28374f9d7fcf1e48ef2fa
paulo07marcena <paulo.marcena@sptech.school>
2023-10-04 09:31:31 -0300

    Layout tela inicial do dashboard analista

--- site/public/dashboard/js/demo/chart-area-demo.js
+++ site/public/dashboard/js/demo/chart-area-demo.js
@@ -1,48 +1,57 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
+(Chart.defaults.global.defaultFontFamily = "Nunito"),
+  '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = "#858796";
 
 // Area Chart Example
 var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
-  type: 'bar',
+  type: "bar",
   data: {
-    labels: ["BAM-12", "BAM-11", "BAM-12", "BAM-12", "BAM-12", "BAM-12", "BAM-12", "BAM-12",],
-    datasets: [{
-      label: "Incidentes",
-      backgroundColor: [
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-        "rgba(78, 165, 230, 0.5)",
-      ],
-      borderColor: [
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-        "rgba(78, 115, 223, 1)",
-      ],
-      data: [20, 10, 25, 31, 35, 20, 25, 31,],
-    },]
+    labels: [
+      "03:00",
+      "06:00",
+      "09:00",
+      "12:00",
+      "15:00",
+      "18:00",
+      "21:00",
+      "00:00",
+    ],
+    datasets: [
+      {
+        label: "Incidentes",
+        backgroundColor: [
+          "#32bcad",
+          "#32bcad",
+          "#32bcad",
+          "#32bcad",
+          "#32bcad",
+          "#32bcad",
+          "#32bcad",
+          "#32bcad",
+        ],
+        borderColor: [
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+          "rgba(78, 115, 223, 1)",
+        ],
+        data: [15, 10, 12, 5, 5, 8, 3, 5],
+      },
+    ],
   },
   options: {
     scales: {
       y: {
-        beginAtZero: true
+        beginAtZero: true,
       },
     },
     maintainAspectRatio: false,
@@ -51,26 +60,26 @@ var myLineChart = new Chart(ctx, {
         left: 10,
         right: 25,
         top: 25,
-        bottom: 0
-      }
+        bottom: 0,
+      },
     },
     legend: {
-      display: false
+      display: false,
     },
     tooltips: {
       backgroundColor: "rgb(255,255,255)",
       bodyFontColor: "#858796",
       titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
+      titleFontColor: "#6e707e",
       titleFontSize: 14,
-      borderColor: '#dddfeb',
+      borderColor: "#dddfeb",
       borderWidth: 1,
       xPadding: 15,
       yPadding: 15,
       displayColors: false,
       intersect: false,
-      mode: 'index',
+      mode: "index",
       caretPadding: 10,
-    }
-  }
+    },
+  },
 });
