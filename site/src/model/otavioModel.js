d32f6b65fe8fa6706100d4d902b25201d7bcdb92
OtavioWalk <otavio.carvalho@sptech.school>
2023-12-01 01:10:33 -0300

    Avanço no back do grafico em tempo real

--- site/src/model/otavioModel.js
+++ site/src/model/otavioModel.js
@@ -17,19 +17,103 @@ FROM
     JOIN Eyes_On_Server.Medida m ON cm.fk_medida = m.id_medida
     JOIN Eyes_On_Server.Servidor s ON cs.fk_servidor = s.id_servidor
 WHERE
-    s.nome_servidor = 'teste suervidor'
-	AND m.nome_medida = 'PorcentagemUso'
+    s.id_servidor = ${fkEmpresa}
     AND c.nome_componente = 'Memoria'
+    AND m.nome_medida = 'PorcentagemUso'
 ORDER BY
     r.momento_registro DESC
 LIMIT 10;
     `
 
-    // SELECT * FROM Eyes_On_Server.Usuario where fk_empresa = ${fkEmpresa};
-    // select* from Eyes_On_Server.view_componentes_servidores order by servidor;
-    // info("grafico()", query);
+//     var query2 = `
+//     SELECT
+//     s.nome_servidor AS Servidor,
+//     c.nome_componente AS Componente,
+//     m.nome_medida AS Medida,
+//     r.valor_registro AS Valor,
+//     DATE_FORMAT(r.momento_registro, '%H:%i:%s') AS Horario_Captura
+// FROM
+//     Eyes_On_Server.Registro r
+//     JOIN Eyes_On_Server.Componente_Servidor cs ON r.fk_componente_servidor = cs.id_componente_servidor
+//     JOIN Eyes_On_Server.Componente_Medida cm ON cs.fk_componente_medida = cm.id_componente_medida
+//     JOIN Eyes_On_Server.Componente c ON cm.fk_componente = c.id_componente
+//     JOIN Eyes_On_Server.Medida m ON cm.fk_medida = m.id_medida
+//     JOIN Eyes_On_Server.Servidor s ON cs.fk_servidor = s.id_servidor
+// WHERE
+//     s.id_servidor = ${fkEmpresa}
+//     AND c.nome_componente = 'Memoria'
+//     AND m.nome_medida = 'PorcentagemUso'
+// ORDER BY
+//     r.momento_registro DESC
+// LIMIT 10;
+//     `
+
+// console.log(query)
+
+
     return bancoDados.executar(query);
 }
+
+
+function graficoCPU(fkEmpresa) {
+
+    var query = `
+    SELECT
+    s.nome_servidor AS Servidor,
+    c.nome_componente AS Componente,
+    m.nome_medida AS Medida,
+    r.valor_registro AS Valor,
+    DATE_FORMAT(r.momento_registro, '%H:%i:%s') AS Horario_Captura
+FROM
+    Eyes_On_Server.Registro r
+    JOIN Eyes_On_Server.Componente_Servidor cs ON r.fk_componente_servidor = cs.id_componente_servidor
+    JOIN Eyes_On_Server.Componente_Medida cm ON cs.fk_componente_medida = cm.id_componente_medida
+    JOIN Eyes_On_Server.Componente c ON cm.fk_componente = c.id_componente
+    JOIN Eyes_On_Server.Medida m ON cm.fk_medida = m.id_medida
+    JOIN Eyes_On_Server.Servidor s ON cs.fk_servidor = s.id_servidor
+WHERE
+    s.id_servidor = ${fkEmpresa}
+    AND c.nome_componente = 'Cpu'
+    AND m.nome_medida = 'PorcentagemUso'
+ORDER BY
+    r.momento_registro DESC
+LIMIT 10;
+    `
+
+
+    return bancoDados.executar(query);
+}
+
+
+function graficoDisco(fkEmpresa) {
+
+    var query = `
+    SELECT
+    s.nome_servidor AS Servidor,
+    c.nome_componente AS Componente,
+    m.nome_medida AS Medida,
+    r.valor_registro AS Valor,
+    DATE_FORMAT(r.momento_registro, '%H:%i:%s') AS Horario_Captura
+FROM
+    Eyes_On_Server.Registro r
+    JOIN Eyes_On_Server.Componente_Servidor cs ON r.fk_componente_servidor = cs.id_componente_servidor
+    JOIN Eyes_On_Server.Componente_Medida cm ON cs.fk_componente_medida = cm.id_componente_medida
+    JOIN Eyes_On_Server.Componente c ON cm.fk_componente = c.id_componente
+    JOIN Eyes_On_Server.Medida m ON cm.fk_medida = m.id_medida
+    JOIN Eyes_On_Server.Servidor s ON cs.fk_servidor = s.id_servidor
+WHERE
+    s.id_servidor = ${fkEmpresa}
+    AND c.nome_componente = 'Disco'
+    AND m.nome_medida = 'PorcentagemUso'
+ORDER BY
+    r.momento_registro DESC
+LIMIT 10;
+    `
+
+
+    return bancoDados.executar(query);
+}
+
 
 function buscarInfo(select) {
     // info("buscarInfo", select);
@@ -46,6 +130,8 @@ function setDados(select) {
 
 module.exports = {
     grafico,
+    graficoCPU,
+    graficoDisco,
     buscarInfo,
     setDados
 };
