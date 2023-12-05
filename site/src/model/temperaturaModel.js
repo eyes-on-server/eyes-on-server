eb8f67bc719f7eb674aa93a1664bad9a4237adfd
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2023-12-05 04:59:52 -0300

    correcao das queries

--- site/src/model/temperaturaModel.js
+++ site/src/model/temperaturaModel.js
@@ -2,29 +2,25 @@ const bancoDados = require("../database/config");
 
 function dadosTemperaturaPorServidor(fkServidor) {
   let query = `SELECT * FROM View_Registros WHERE Servidor = ${fkServidor} AND Medida = 'Temperatura' 
-    ORDER BY momento_registro DESC
-    OFFSET 0 ROWS FETCH FIRST 100 ROWS ONLY;
+    ORDER BY momento_registro DESC LIMIT 100;
     `;
   return bancoDados.executar(query);
 }
 function dadosUsoCpuPorServidor(fkServidor) {
-  let query = `SELECT * FROM View_Registros WHERE Servidor = ${fkServidor} AND Componente = 'Cpu' LIMIT 100
-    ORDER BY momento_registro DESC
-    OFFSET 0 ROWS FETCH FIRST 100 ROWS ONLY;
+  let query = `SELECT * FROM View_Registros WHERE Servidor = ${fkServidor} AND Componente = 'Cpu'
+    ORDER BY momento_registro DESC LIMIT 100;
     `;
   return bancoDados.executar(query);
 }
 function dadosUsoMemPorServidor(fkServidor) {
-  let query = `SELECT * FROM View_Registros WHERE Servidor = ${fkServidor} AND Componente = 'Memoria' LIMIT 100
-    ORDER BY momento_registro DESC
-    OFFSET 0 ROWS FETCH FIRST 100 ROWS ONLY;
+  let query = `SELECT * FROM View_Registros WHERE Servidor = ${fkServidor} AND Componente = 'Memoria'
+    ORDER BY momento_registro DESC LIMIT 100;
     `;
   return bancoDados.executar(query);
 }
 function dadosUsoDiscoPorServidor(fkServidor) {
-  let query = `SELECT * FROM View_Registros WHERE Servidor = ${fkServidor} AND Componente = 'Disco' LIMIT 100
-    ORDER BY momento_registro DESC
-    OFFSET 0 ROWS FETCH FIRST 100 ROWS ONLY;
+  let query = `SELECT * FROM View_Registros WHERE Servidor = ${fkServidor} AND Componente = 'Disco'
+    ORDER BY momento_registro DESC LIMIT 100;
     `;
   return bancoDados.executar(query);
 }
